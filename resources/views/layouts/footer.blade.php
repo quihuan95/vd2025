ea746c02eeec58261d8711371da9f50fc46ac3f9
PhamMinh <webmaster2.hbg@gmail.com>
2025-09-30 18:45:51 +0700

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
       <!-- Copyright -->
       <div class='fModule f-copyright py-2 text-center'>
         <p>
-          <span>Copyright {{ date('Y') }} WCES - All Rights Reserved.</span>
+          <span>Copyright {{ date('Y') }} VDUHSC - All Rights Reserved.</span>
           <span>Powered By <a href="https://events.anderesfourdy.com/" target="_blank" rel="noopener noreferrer">Anderes Fourdy</a> | </span>
           <a href="#" title="Privacy Policy"><span>Privacy Policy</span></a>
         </p>
